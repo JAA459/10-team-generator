48aa7fc38daf523c341972cd899205041fe110c0
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-01-30 20:57:26 -0700

    working application and added css page

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -9,8 +9,143 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const team = [];
 
+function confirm() {
+    inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'confirmMember',
+                message: 'Would you like to add a new team member?',
+            },
+        ])
+        .then((data) => {
+            if (data.confirmMember == true) {
+                addMember();
+            } else if (data.confirmMember == false) {
+                renderHtml();
+            }
+        })
+}
 
+function addMember() {
+    inquirer
+        .prompt([
+            {
+                type: 'checkbox',
+                name: 'employeeType',
+                message: 'what is your job title?',
+                choices: ["manager", "engineer", "intern"]
+            },
+        ])
+        .then((data) => {
+            const title = data.employeeType.toString();
+
+            if (title === "manager") {
+                getManager();
+            } else if (title === "engineer") {
+                getEngineer();
+            } else if (title === "intern") {
+                getIntern();
+            }
+        })
+}
+
+function getManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office Number?',
+        },
+    ])
+        .then((data) => {
+            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            team.push(manager);
+            confirm();
+        })
+}
+
+function getIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school are you attending?',
+        },
+    ])
+        .then((data) => {
+            const intern = new Intern(data.name, data.id, data.email, data.school)
+            team.push(intern);
+            confirm();
+        })
+}
+    function getEngineer() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your github username?',
+            },
+        ])
+        .then((data) => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github)
+            team.push(engineer);
+            confirm();
+        })
+    }
+
+       function renderHtml() {
+            fs.writeFileSync(outputPath, render(team), "utf-8");
+        };
+    addMember();
+        
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
